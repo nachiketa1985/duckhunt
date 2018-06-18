f58ee409785adbc44a63418fd94d66a34cb248fd
aa <hypercodeplace@gmail.com>
2018-06-18 17:34:45 +0500

    Remove redundant code.

--- DuckHunt.Web/Models/ErrorViewModel.cs
+++ DuckHunt.Web/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace DuckHunt.Web.Models
 {
     public class ErrorViewModel
--- DuckHunt.Web/Program.cs
+++ DuckHunt.Web/Program.cs
@@ -7,12 +7,10 @@ namespace DuckHunt.Web
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build()
+                .Run();
+        }
     }
 }
